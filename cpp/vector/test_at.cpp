52da169948e9ae8692da40ec15cd1cea82580590
cyber-pioneer <chenzhuo@tju.edu.cn>
2023-12-22 03:37:20 +0000

    add vector case at

---
+++ cpp/vector/test_at.cpp
@@ -0,0 +1,14 @@
+#include<vector>
+#include<iostream>
+
+int main(){
+    std::vector<int> aa{1,2,3};
+    aa[0] = 5;
+    aa.at(1) = 10;
+    std::cout<<"out of range vector [] " << aa[4]<<std::endl;
+    std::cout<<"out of range vector at " << aa.at(4)<<std::endl;
+    return 0;
+}
+
+// 1. 异常处理：当访问超出范围的索引时，[]操作符不会进行任何检查，也不会抛出异常。而at函数会检查索引是否超出范围，如果超出范围，会抛出std::out_of_range异常。
+// 2. 性能：由于[]操作符不需要进行边界检查，因此在访问元素时通常比at函数更快一些。
