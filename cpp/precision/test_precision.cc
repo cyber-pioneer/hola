dae8700deadf09f21404e3723ea9edbaa6514700
cyber-pioneer <chenzhuo@tju.edu.cn>
2024-06-05 12:10:06 +0000

    add precision trunc

--- cpp/precision/test_precision.cc
+++ cpp/precision/test_precision.cc
@@ -15,17 +15,27 @@ template <typename T> bool isCloseEqualMax_value(T const value) {
 }
 } // namespace
 
+float truncateInfinityAndMaxValue(float value, float maxValue) {
+  if (std::isinf(value) || value > maxValue) {
+    return maxValue;
+  }
+  return value;
+}
+
 int main() {
-  float v = std::numeric_limits<float>::max();
+  float v = std::numeric_limits<float>::lowest();
   auto res = isCloseEqualMax_value<float>(v);
+  float tt = -float(std::numeric_limits<double>::max());
+
+  std::cout << " tt>v " << (tt < v) << " " << v << std::endl;
 
   std::cout << " " << res << std::endl;
   std::ostringstream ss;
-  if (res)
-    std::fesetround(FE_TOWARDZERO); //朝0近似，正数会截断
+  // if (res)
+  std::fesetround(FE_TOWARDZERO); //朝0近似，正数会截断
   ss << std::setprecision(std::numeric_limits<float>::max_digits10);
   ss << std::showpoint;
-  ss << v;
+  ss << truncateInfinityAndMaxValue(tt, v);
   std::string result = ss.str();
   std::cout << result << std::endl;
   return 0;
