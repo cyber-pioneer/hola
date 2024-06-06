5b02e91715c77582727c0ae7d2770f6a3ddeafd9
cyber-pioneer <chenzhuo@tju.edu.cn>
2024-06-06 03:26:28 +0000

    add compiler

---
+++ cpp/compiler/source.cc
@@ -0,0 +1,10 @@
+#include "tmp.h"
+#include <iostream>
+
+#define AA 3
+
+int main() {
+  fn(AA);
+  std::cout << "Hello World! AA" << std::endl;
+  return 0;
+}
---
+++ cpp/compiler/tmp.h
@@ -0,0 +1,3 @@
+#include <iostream>
+
+inline void fn(int x) { std::cout << "call tmp.h " << x << std::endl; }
