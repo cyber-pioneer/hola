971d9cf2f46285f8ca81ff7d88afbc190191ac38
pioneer <pioneer@mac.lan>
2024-06-24 12:35:41 +0800

    update rank

--- cpp/lc/rank/test_quick_rank.cc
+++ cpp/lc/rank/test_quick_rank.cc
@@ -12,36 +12,40 @@ void check(std::vector<int> ref, std::vector<int> actual) {
   }
 }
 
-void dfs(std::vector<int> &nums, int l_n, int r_n) {
-  if (l_n >= r_n)
+void dfs(std::vector<int> &nums, int l1, int r1) {
+  if (l1 >= r1)
     return;
-  int l = l_n;
-  int r = r_n;
+  int l = l1;
+  int r = r1;
   int mid = l;
-  while (l < r) {
-    if (nums[r] >= nums[mid]) {
-      r--;
-    } else if (nums[l] <= nums[mid]) {
-      l++;
-    } else if (nums[l] > nums[r]) {
-      std::swap(nums[l], nums[r]);
-    }
-  }
-  std::swap(nums[l], nums[mid]);
 
-  dfs(nums, l_n, mid);
-  dfs(nums, mid + 1, r_n);
+  while (l<r) {
+    int mid_v = nums[mid];
+    if (nums[mid] <= nums[r]) {
+      r --;
+    } else if (nums[l] < nums[mid]) {
+      l ++;
+    } else if (nums[l] > nums[r]){
+      std::swap(nums[l], nums[r]);
+    } 
+
+  }
+  std::swap(nums[mid], nums[l]);
+
+  dfs(nums, l1, mid-1);
+  dfs(nums, mid + 1, r1);
 }
 
 int main() {
-  std::vector<int> org = {2, 6, 5, 3, 3, 7};
+  std::vector<int> org = {5, 2, 3, 3, 7};
 
   auto ref = org;
   std::sort(ref.begin(), ref.end());
   dfs(org, 0, org.size() - 1);
-  // check(org, ref);
+  check(org, ref);
   for (auto x : org) {
     std::cout << x << " ";
   }
+  std::cout<<std::endl;
   return 0;
 }
