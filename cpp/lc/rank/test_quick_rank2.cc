3c66b443166491bd4cf09486caa16a50ba334cce
cyber-pioneer <chenzhuo@tju.edu.cn>
2024-07-02 03:43:01 +0000

    update quick rank

--- cpp/lc/rank/test_quick_rank2.cc
+++ cpp/lc/rank/test_quick_rank2.cc
@@ -8,15 +8,25 @@ void dfs(std::vector<int> &res, int l, int r) {
   int i = l;
   int j = r;
   int tar_idx = l; // any index
-  int tmp = res[tar_idx];
+  int tar = res[tar_idx];
+  std::cout << "** " << i << j << std::endl;
+  for (int m = i; m <= j; m++) {
+    std::cout << res[m] << " " << std::endl;
+  }
   while (i < j) {
-    while (res[j] >= tmp && i < j) { // if not >=, 5,2,3,4,5, swap(5, 5); // if
-                                     // i<=j ,i++ may > j or i may > j--
+    while (res[j] >= tar && i < j) {
+      // if not >=, 5,2,3,4,5, swap(5, 5); 确保j对应的值小于tar
+      // if i<=j ,i++ may > j or i may > j--
+      // 先右后左，对于[4,2,1,0]，i,j[0,3] --> [0,2,1,4] -->i=2,j=2
+      // 若先左后右，对于[4,2,1,0]，i,j[0,3]
+      // -->[0,2,1,4]-->i=3,j=3-->dfs(res,0,3)死循环
       j--;
     }
-    while (res[i] < tmp && i < j) {
+    while (res[i] < tar && i < j) {
       i++;
     }
+    std::cout << "== " << i << j << std::endl;
+
     std::swap(res[j], res[i]);
   }
   std::swap(res[tar_idx], res[i]);
@@ -25,7 +35,8 @@ void dfs(std::vector<int> &res, int l, int r) {
 }
 
 int main() {
-  std::vector<int> v = {3, 6, 4, 1, 3, 7, 9, 2, 4, 5, 0};
+  // std::vector<int> v = {3, 6, 4, 1, 3, 7, 9, 2, 4, 5, 0};
+  std::vector<int> v = {4, 2, 1, 0};
   dfs(v, 0, v.size() - 1);
   for (int &i : v) {
     std::cout << i << " ";
