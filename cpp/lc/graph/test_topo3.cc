d93866c923aadc5f77c79e85de630fe3271f279b
cyber-pioneer <chenzhuo@tju.edu.cn>
2024-07-17 06:28:30 +0000

    polish topo case

--- cpp/lc/graph/test_topo3.cc
+++ cpp/lc/graph/test_topo3.cc
@@ -5,8 +5,8 @@
 #include <unordered_map>
 #include <vector>
 
-std::unordered_map<int, int> key_freq;
-std::unordered_map<int, std::set<int>> freq_keys;
+std::unordered_map<int, int> key_indegree;
+std::unordered_map<int, std::set<int>> indegree_keys;
 std::unordered_map<int, std::set<int>> key_nexts;
 std::list<std::pair<int, int>> link;
 
@@ -17,28 +17,39 @@ void prepare() {
     int src = it.first;
     int next = it.second;
     key_nexts[src].insert(next);
-    if (!key_freq.count(src)) {
-      key_freq[src] = 0;
-      freq_keys[0].insert(src);
+    if (!key_indegree.count(src)) {
+      key_indegree[src] = 0;
+      indegree_keys[0].insert(src);
     }
-    if (key_freq.count(next)) {
-      int cur_freq = key_freq[next];
-      freq_keys[cur_freq].erase(next);
-      freq_keys[cur_freq + 1].insert(next);
-      key_freq[next] = cur_freq + 1;
+    if (key_indegree.count(next)) {
+      int cur_indegree = key_indegree[next];
+      indegree_keys[cur_indegree].erase(next);
+      indegree_keys[cur_indegree + 1].insert(next);
+      key_indegree[next] = cur_indegree + 1;
     } else {
-      key_freq[next] = 1;
-      freq_keys[1].insert(next);
+      key_indegree[next] = 1;
+      indegree_keys[1].insert(next);
     }
   }
 }
 
 void sort(std::set<int> nodes) {
-  std::cout << key_freq.size() << freq_keys.size() << key_nexts.size()
-            << std::endl;
   if (!nodes.size()) {
-    if (res.size() != key_freq.size()) {
+    if (res.size() != key_indegree.size()) {
       std::cout << "exist cycle ***** " << std::endl;
+      std::cout << "finished nodes: ";
+      for (auto val : res) {
+        std::cout << val << " ";
+      }
+      std::cout << std::endl;
+      std::cout << "remained nodes: " << std::endl;
+      for (auto it : indegree_keys) {
+        std::cout << "indegree: node  " << it.first << " : ";
+        for (auto remain : it.second) {
+          std::cout << remain << " ";
+        }
+        std::cout << std::endl;
+      }
     }
     return;
   };
@@ -48,27 +59,26 @@ void sort(std::set<int> nodes) {
     if (key_nexts.count(node)) {
 
       for (auto next : key_nexts[node]) {
-        int cur_freq = key_freq[next];
-        key_freq[next] = cur_freq - 1;
-        freq_keys[cur_freq].erase(next);
-        freq_keys[cur_freq - 1].insert(next);
+        int cur_indegree = key_indegree[next];
+        key_indegree[next] = cur_indegree - 1;
+        indegree_keys[cur_indegree].erase(next);
+        indegree_keys[cur_indegree - 1].insert(next);
       }
     }
-    freq_keys[0].erase(node);
+    indegree_keys[0].erase(node);
   }
 
-  sort(freq_keys[0]);
+  sort(indegree_keys[0]);
 }
 
 int main() {
-  link = {{1, 2}, {1, 3}, {3, 2}, {2, 6}, {3, 6}, {2, 3}};
+  // src-->next
+  // link = {{1, 2}, {1, 3}, {3, 2}, {2, 6}, {3, 6}, {2, 3}};
+  link = {{1, 2}, {1, 3}, {3, 2}, {2, 6}, {3, 6}};
   prepare();
-  for (auto item : res) {
-    std::cout << item << " ";
-  }
-  std::cout << std::endl;
 
-  sort(freq_keys[0]);
+  sort(indegree_keys[0]);
+  std::cout << "topo sort res: ";
   for (auto item : res) {
     std::cout << item << " ";
   }
